ca1a984317f0cbf6a51c33f3fb0be73232877c3a
Farah <farah.assgari@gmail.com>
2023-12-08 16:04:00 -0500

    added form to the contact page

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,10 +1,17 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState } from 'react';
 import './ContactPage.css';
 
 const Contact = () => {
+  const [name, setName] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    //alert(`The name you entered was: ${name}`)
+    document.getElementById ("contact-response").innerHTML =`Thanks, ${name}! We will get back to you soon`
+  }
 
   useEffect(() => {
-    document.title = "React Weather | Contact"   }, []);
+    document.title = "React Weather | Contact" }, []);
 
   return (
     <div className="app">
@@ -12,8 +19,19 @@ const Contact = () => {
       <div className= "text-content">
         <div>
           <p>Contact us if you have questions:</p>
-          <br></br>
         </div>
+        <form onSubmit={handleSubmit}>
+          <label>Enter your name:
+            <input 
+              type="text" 
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+          <input type="submit" />
+        </form>
+        <p id="contact-response"></p>
+        <div>Connect with us on linkedIn</div>
       </div>
     </div>
   )
