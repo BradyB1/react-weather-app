7a96274363204af52ba37df951101c3f0bfd7036
BradyB1 <117840393+BradyB1@users.noreply.github.com>
2023-12-08 11:35:17 -0500

    made the nav bar attach to the bottom of the screen and responds to the height of the screen (mostly-effective)

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,6 +1,65 @@
+// import './NavBar.css';
+
+// const Navbar = () => {
+//   return (
+//     <>
+//       <nav className="navbar">
+//         <ul>
+//           <li>
+//             <a href="/">Home</a>
+//           </li>
+//           <li>
+//             <a href="/about">About</a>
+//           </li>
+//           <li>
+//             <a href="/contact">Contact</a>
+//           </li>
+//         </ul>
+//       </nav>
+//     </>
+//   )
+// };
+
+// export default Navbar;
+
+// new script incase we need to revert back to the old one
+import React, { useEffect, useState } from 'react';
 import './NavBar.css';
 
 const Navbar = () => {
+  const [contentHeight, setContentHeight] = useState(0);
+  // Adjust the maximum padding as needed
+  const maxPadding = 350; 
+
+  const updatePadding = () => {
+    const navbarHeight = document.querySelector('.navbar').offsetHeight;
+    const newPadding = Math.min(navbarHeight + contentHeight, maxPadding);
+    document.body.style.paddingBottom = `${newPadding}px`;
+  };
+
+  useEffect(() => {
+    const updateContentHeight = () => {
+      // Change selector based on your content structure
+      const content = document.querySelector('#root'); 
+      setContentHeight(content.offsetHeight);
+    };
+
+    updatePadding(); 
+    updateContentHeight(); 
+
+    window.addEventListener('resize', () => {
+      updatePadding();
+      updateContentHeight();
+    });
+
+    return () => {
+      window.removeEventListener('resize', () => {
+        updatePadding();
+        updateContentHeight();
+      });
+    };
+  }, [contentHeight]);
+
   return (
     <>
       <nav className="navbar">
@@ -17,7 +76,7 @@ const Navbar = () => {
         </ul>
       </nav>
     </>
-  )
+  );
 };
 
 export default Navbar;
