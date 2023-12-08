05fbfc7600ec3d2ab1ac8d81092d0e84ac5bf5c9
JamesMille02 <144379986+JamesMille02@users.noreply.github.com>
2023-12-07 22:30:19 -0500

    added location loading
    Added the ability to load current user location, if accepted by user.  It displays the information.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,138 +1,191 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../index.css';
 import axios from 'axios';
 
-
 const Home = () => {
+  const [location, setLocation] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [weatherData, setWeatherData] = useState(null);
 
-    const [data, setData] = useState({})
-    const [location, setLocation] = useState("")
-  
-    const api_key = "438f4f20a95048ebb8bbf12f71594554"
-  
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=${api_key}`
-  
-  
-  
-    const searchLocation = (event) => {
-      if (event.key === 'Enter') {
-        axios.get(url).then((response) => {
-          setData(response.data)
-          console.log(response.data)
+  useEffect(() => {
+    const api_key = '438f4f20a95048ebb8bbf12f71594554';
+
+    const getWeatherData = (latitude, longitude) => {
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${api_key}`;
+
+      axios
+        .get(url)
+        .then((response) => {
+          setWeatherData(response.data);
+          setLoading(false);
+          setError(null);
         })
-        setLocation("")
-      }
+        .catch((error) => {
+          console.error('Error fetching weather data:', error);
+          setLoading(false);
+          setError('Error fetching weather data. Please try again.');
+        });
+    };
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const latitude = position.coords.latitude;
+          const longitude = position.coords.longitude;
+
+          getWeatherData(latitude, longitude);
+        },
+        (error) => {
+          console.error(`Error getting geolocation: ${error.message}`);
+          setLoading(false);
+          setError('Error getting geolocation. Please enter a location manually.');
+        }
+      );
+    } else {
+      console.error('Geolocation is not supported by this browser.');
+      setLoading(false);
+      setError('Geolocation is not supported. Please enter a location manually.');
     }
-  
-    
-    return (
-      <div className="app">
-                
-          <div className="search">
-          
-            <input
-              value={location}
-              onChange={event => setLocation(event.target.value)}
-              onKeyDown={searchLocation}
-              placeholder='Enter Location'
-              type="text" />
-  
+  }, []);
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      setLoading(true);
+      const api_key = '438f4f20a95048ebb8bbf12f71594554';
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=${api_key}`;
+
+      axios
+        .get(url)
+        .then((response) => {
+          setWeatherData(response.data);
+          setLoading(false);
+          setError(null);
+        })
+        .catch((error) => {
+          console.error('Error fetching weather data:', error);
+          setLoading(false);
+          setError('Location not found. Please try again.');
+        });
+
+      setLocation('');
+    }
+  };
+
+  return (
+    <div className="app">
+      <div className="search">
+        <input
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          onKeyDown={searchLocation}
+          placeholder="Enter Location"
+          type="text"
+        />
+      </div>
+
+      <div className="container">
+        {loading ? (
+          <p>Loading...</p>
+        ) : error ? (
+          <p>{error}</p>
+        ) : (
+          <div>
+            <div className="top">
+              <div className="location">
+                <p>{weatherData.name}</p>
+              </div>
+              <div className="temp">
+                {weatherData.main ? <h1>{weatherData.main.temp.toFixed()}°F</h1> : null}
+              </div>
+              <div className="description">
+                {weatherData.weather ? <p>{weatherData.weather[0].main}</p> : null}
+              </div>
+            </div>
+
+            <div className="data-box-container">
+              <div className="feels-box">
+                <div className="feels">
+                  {weatherData.main ? (
+                    <p className="bold">{weatherData.main.feels_like.toFixed()}°F</p>
+                  ) : null}
+                  <p>Feels Like</p>
+                </div>
+              </div>
+
+              <div className="humidity-box">
+                <div className="humidity">
+                  {weatherData.main ? (
+                    <p className="bold">{weatherData.main.humidity}%</p>
+                  ) : null}
+                  <p>Humidity</p>
+                </div>
+              </div>
+
+              <div className="wind-box">
+                <div className="wind">
+                  {weatherData.wind ? (
+                    <p className="bold">{weatherData.wind.speed.toFixed()} MPH</p>
+                  ) : null}
+                  <p>Wind Speed</p>
+                </div>
+              </div>
+
+              <div className="clouds-box">
+                <div className="clouds">
+                  {weatherData.clouds ? (
+                    <p className="bold">{weatherData.clouds.all}%</p>
+                  ) : null}
+                  <p>Cloudiness</p>
+                </div>
+              </div>
+
+              <div className="min-max-box">
+                <div className="min-max">
+                  {weatherData.main ? (
+                    <p className="bold">
+                      {weatherData.main.temp_min.toFixed()}°F-{weatherData.main.temp_max.toFixed()}°F
+                    </p>
+                  ) : null}
+                  <p>Min-Max</p>
+                </div>
+              </div>
+
+              <div className="sunrise-box">
+                <div className="sunrise">
+                  {weatherData.sys ? (
+                    <p className="bold">
+                      {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString([], {
+                        hour: 'numeric',
+                        minute: '2-digit',
+                        hour12: true,
+                      })}
+                    </p>
+                  ) : null}
+                  <p>Sunrise</p>
+                </div>
+              </div>
+
+              <div className="sunset-box">
+                <div className="sunset">
+                  {weatherData.sys ? (
+                    <p className="bold">
+                      {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString([], {
+                        hour: 'numeric',
+                        minute: '2-digit',
+                        hour12: true,
+                      })}
+                    </p>
+                  ) : null}
+                  <p>Sunset</p>
+                </div>
+              </div>
+            </div>
           </div>
-        <div className="container">
-          <div className="top">
-            <div className="location">
-              <p>{data.name}</p>
-            </div>
-            <div className="temp">
-              {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
-  
-            </div>
-            <div className="description">
-              {data.weather ? <p>{data.weather[0].main}</p> : null}
-            </div>
-          </div>
-          {/* this  */}
-          {data.name !== undefined && (
-    
-    <div className="data-box-container">
-      <div className="feels-box">
-        <div className="feels">
-          {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F </p> : null}
-          <p>Feels Like</p>
-        </div>
+        )}
       </div>
-  
-      <div className="humidity-box">
-        <div className="humidity">
-          {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
-          <p>Humidity</p>
-        </div>
-      </div>
-  
-      <div className="wind-box">
-        <div className="wind">
-          {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
-          <p>Wind Speed</p>
-        </div>
-        </div>
-      <div className="clouds-box">
-        <div className="clouds">
-          {data.clouds ? <p className='bold'> {data.clouds.all}%</p> : null}
-          <p>Cloudiness</p>
-        </div>
-      </div>
-      <div className="min-max-box">
-        <div className="min-max">
-          {data.main ? <p className ='bold'>{data.main.temp_min.toFixed()}°F-{data.main.temp_max.toFixed()}°F</p> : null}
-          <p>Min-Max</p>
-        </div>
-      </div>
-      <div className="sunrise-box">
-        <div className="sunrise">
-          {data.sys ?( <p className= 'bold'> {new Date(data.sys.sunrise * 1000).toLocaleTimeString([], {hour: 'numeric', minute: '2-digit', hour12: true})}</p>) : null}
-          <p>Sunrise</p>
-  
-        </div>
-      </div>
-      <div className="sunset-box">
-        <div className="sunset">
-          {data.sys ?( <p className= 'bold'> {new Date(data.sys.sunset * 1000).toLocaleTimeString([],{hour: 'numeric', minute: '2-digit', hour12: true})}</p>) : null}
-          <p>Sunset</p>
-  
-        </div>
-      </div>
-      </div>
-  )}
-  
-  
-  {/* to this */}
-          {data.name !== undefined &&
-            <div className="bottom">
-              {/* <div className="feels">
-                {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
-                <p>Feels Like</p>
-              </div> */}
-              {/* <div className="humidity">
-                {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
-                <p>Humidity</p>
-              </div> */}
-              {/* <div className="wind">
-                {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
-  
-                <p>Wind Speed</p>
-              </div> */}
-              {/* <div className="min-max">
-              {data.main ? <p className ='bold'>{data.main.temp_min.toFixed()}°F-{data.main.temp_max.toFixed()}°F</p> : null}
-              <p>Min-Max</p>
-              </div> */}
-            </div>
-          }
-  
-        </div>
-  
-      </div>
-    );
-  
-}
+    </div>
+  );
+};
 
 export default Home;
