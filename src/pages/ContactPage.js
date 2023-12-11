794d5518a5a269dbbbca2b38afe4f4825be5f854
BradyB1 <117840393+BradyB1@users.noreply.github.com>
2023-12-11 10:00:33 -0500

    feat(added the email box, message box, and linkedin links to the contact page. Added styling as well to make the linkedin li items buttons.)

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,37 +1,64 @@
-import React, {useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './ContactPage.css';
 
 const Contact = () => {
   const [name, setName] = useState("");
-
+  const [email,setEmail] = useState("");
+  const [message, setMessage] = useState(""); 
   const handleSubmit = (event) => {
     event.preventDefault();
-    //alert(`The name you entered was: ${name}`)
-    document.getElementById ("contact-response").innerHTML =`Thanks, ${name}! We will get back to you soon`
+    document.getElementById("contact-response").innerHTML = `Thanks, ${name}! We will get back to you via email: ${email}. Your message to us was: ${message}`;
   }
 
   useEffect(() => {
-    document.title = "React Weather | Contact" }, []);
+    document.title = "React Weather | Contact"
+  }, []);
 
   return (
     <div className="app">
       <h1>Contact Us</h1>
-      <div className= "text-content">
+      <div className="text-content">
         <div>
           <p>Contact us if you have questions:</p>
+        
         </div>
         <form onSubmit={handleSubmit}>
+          {/* collects name */}
           <label>Enter your name:
-            <input 
-              type="text" 
+            <input
+              type="text"
               value={name}
               onChange={(e) => setName(e.target.value)}
             />
           </label>
+          {/* collects email */}
+          <label>Enter your email:
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+
+          {/* collects message */}
+          <label>Enter your message:
+            <textarea
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+          </label>
+
           <input type="submit" />
         </form>
         <p id="contact-response"></p>
-        <div>Connect with us on linkedIn</div>
+        <div>Connect with us on LinkedIn</div>
+        <div>
+        <ul className = "linkedin-links">
+              <li> <a href='https://www.linkedin.com/in/brady-buttrey-2b873a140/'>Brady Buttrey</a> </li>
+              <li><a href = ''>Farah</a></li>
+              <li> <a href = 'https://www.linkedin.com/in/james-miller-649768222/'>James Miller</a></li>
+            </ul>
+        </div>
       </div>
     </div>
   )
